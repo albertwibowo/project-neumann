37db705a43749455530c59acdf6ee3d3c265e208
albertwibowo <albert14wibowo@gmail.com>
2024-02-23 10:01:21 +0000

    updated app

--- app.py
+++ app.py
@@ -50,7 +50,8 @@ tab0.subheader("Notes")
 tab0.markdown(
     """
 * Ensure each column in a dataframe has the right data format before being uploaded - the data type detection in the app is not perfect
-* Do not upload datetime columns - this will cause error 
+* DO NOT upload datetime columns - this will cause error 
+* Ensure dataframe does **NOT** contain **ANY** missing values
 * The algorithm will ignore any column that **IS NOT** present in the reference dataframe
 """
 )
@@ -62,7 +63,6 @@ tab0.markdown(
 tab1.markdown("###### Upload dataframes")
 target_file = tab1.file_uploader("Choose a target dataframe")
 reference_file = tab1.file_uploader("Choose a reference dataframe")
-
 if target_file is not None and reference_file is not None:
     target_df = pd.read_csv(target_file)
     reference_df = pd.read_csv(reference_file)
@@ -83,15 +83,16 @@ if target_file is not None and reference_file is not None:
     tab1.markdown("###### Column types")
     tab1.dataframe(dh.type_df)
 
+
 # Tab 2 - Analysis
 
 if target_file is not None and reference_file is not None:
     tab2.markdown("###### Analysis result")
     dh.analye(p_threshold)
     tab2.dataframe(dh.result_df)
-
+    
     tab2.markdown("###### Visualisation")
     vis_col = tab2.selectbox(label="select a column to visualise", 
-                             options= dh.type_df['column_names'].unique())
+                             options= dh.type_df[dh.type_df['column_types'] != 'id']['column_names'].unique())
     
     tab2.plotly_chart(dh.visualise(column_name=vis_col), use_container_width=True)
